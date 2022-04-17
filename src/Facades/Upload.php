0af57b1f98e0a888641a0a6d45f6c55183faaa74
zgenius matondo <zgeniuscoders@gmail.com>
2022-04-17 09:25:07 -0700

    Uploads des fichier

---
+++ src/Facades/Upload.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Zgeniuscoders\Zgeniuscoders\Facades;
+
+
+class Upload
+{
+
+}
---
+++ src/File/Upload.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Zgeniuscoders\Zgeniuscoders\File;
+
+
+class Upload
+{
+
+}
